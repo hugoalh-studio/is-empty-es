4e28a553d882b8530acda58558cac059213c76b2
hugoalh <32359235+hugoalh@users.noreply.github.com>
2024-03-12 16:58:53 +0800

    Initial

---
+++ mod.test.ts
@@ -0,0 +1,14 @@
+import { assertEquals } from "TEST/assert_equals.ts";
+import { isEmpty } from "./mod.ts";
+Deno.test("False 1", { permissions: "none" }, () => {
+	assertEquals(isEmpty(true), false);
+});
+Deno.test("False 2", { permissions: "none" }, () => {
+	assertEquals(isEmpty([{}]), false);
+});
+Deno.test("True 1", { permissions: "none" }, () => {
+	assertEquals(isEmpty([]), true);
+});
+Deno.test("True 2", { permissions: "none" }, () => {
+	assertEquals(isEmpty({}), true);
+});
