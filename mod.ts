70603cad719f2fd7232f1a09ff060e3020b2469d
hugoalh <hugoalh@gmail.com>
2024-10-22 12:55:39 +0800

    Update JSDoc

--- mod.ts
+++ mod.ts
@@ -5,6 +5,26 @@ import { isObjectPlain } from "https://raw.githubusercontent.com/hugoalh-studio/
  * This only reports back what the JavaScript engine is seeing; In particular, the return value may not match the original source code if a transpilation tool was used.
  * @param {unknown} item Item that need to determine.
  * @returns {boolean} Determine result.
+ * @example 1
+ * ```ts
+ * isEmpty(true);
+ * //=> false
+ * ```
+ * @example 2
+ * ```ts
+ * isEmpty([{}]);
+ * //=> false
+ * ```
+ * @example 3
+ * ```ts
+ * isEmpty([]);
+ * //=> true
+ * ```
+ * @example 4
+ * ```ts
+ * isEmpty({});
+ * //=> true
+ * ```
  */
 export function isEmpty(item: unknown): boolean {
 	switch (typeof item) {
