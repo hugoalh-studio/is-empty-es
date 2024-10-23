3f4806126afe57a17255be26bb8813add74cbd22
hugoalh <hugoalh@gmail.com>
2024-10-23 15:04:30 +0800

    Update test

--- mod.test.ts
+++ mod.test.ts
@@ -3,11 +3,11 @@ import { isEmpty } from "./mod.ts";
 Deno.test("Boolean `false`", { permissions: "none" }, () => {
 	assertEquals(isEmpty(false), false);
 });
-Deno.test("Array With 1 Plain Object With 0 Entries", { permissions: "none" }, () => {
-	assertEquals(isEmpty([{}]), false);
-});
-Deno.test("Array With 0 Length", { permissions: "none" }, () => {
+Deno.test("Array With 0 Elements", { permissions: "none" }, () => {
 	assertEquals(isEmpty([]), true);
+});
+Deno.test("Array With 1 Element", { permissions: "none" }, () => {
+	assertEquals(isEmpty([{}]), false);
 });
 Deno.test("Plain Object With 0 Entries", { permissions: "none" }, () => {
 	assertEquals(isEmpty({}), true);
