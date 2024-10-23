71d0e943356c8beda9ca54e87fa63b692e4509ba
hugoalh <hugoalh@gmail.com>
2024-10-23 11:08:44 +0800

    Update test

--- mod.test.ts
+++ mod.test.ts
@@ -1,14 +1,20 @@
 import { assertEquals } from "STD/assert/equals";
 import { isEmpty } from "./mod.ts";
-Deno.test("1", { permissions: "none" }, () => {
-	assertEquals(isEmpty(true), false);
+Deno.test("Boolean `false`", { permissions: "none" }, () => {
+	assertEquals(isEmpty(false), false);
 });
-Deno.test("2", { permissions: "none" }, () => {
+Deno.test("Array With 1 Plain Object With 0 Entries", { permissions: "none" }, () => {
 	assertEquals(isEmpty([{}]), false);
 });
-Deno.test("3", { permissions: "none" }, () => {
+Deno.test("Array With 0 Length", { permissions: "none" }, () => {
 	assertEquals(isEmpty([]), true);
 });
-Deno.test("4", { permissions: "none" }, () => {
+Deno.test("Plain Object With 0 Entries", { permissions: "none" }, () => {
 	assertEquals(isEmpty({}), true);
+});
+Deno.test("Map With 0 Entries", { permissions: "none" }, () => {
+	assertEquals(isEmpty(new Map()), true);
+});
+Deno.test("Set With 0 Entries", { permissions: "none" }, () => {
+	assertEquals(isEmpty(new Set()), true);
 });
